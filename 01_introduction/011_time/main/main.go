86edd3ef270834d38e5598290faf5e828322701a
masteryourself <renzhao.ruan@foxmail.com>
2021-11-26 23:44:39 +0800

    时间函数

--- 01_introduction/011_time/main/main.go
+++ 01_introduction/011_time/main/main.go
@@ -18,4 +18,14 @@ func main()  {
 
 	fmt.Printf("当前年月日 %v \n", now.Format("2006/01/02"))
 
+	// 时间单位
+	for i := 0; i < 20; i++ {
+		fmt.Printf("%d \t", i)
+		time.Sleep(100 * time.Millisecond)
+	}
+	fmt.Println()
+
+	// unix 时间戳
+	fmt.Printf("unix 时间戳 = %v, unixNano 时间戳 = %v", now.Unix(), now.UnixNano())
+
 }
