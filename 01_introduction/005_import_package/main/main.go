b7112df2e25d2a18a06b220b17e846bbeeab7e53
masteryourself <renzhao.ruan@foxmail.com>
2021-12-04 17:40:01 +0800

    工厂模式

--- 01_introduction/005_import_package/main/main.go
+++ 01_introduction/005_import_package/main/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"tutorial.masteryourself.pers/tutorial_go/005_import_package/hero"
+	"tutorial.masteryourself.pers/tutorial_go/01_introduction/005_import_package/hero"
 )
 
 func main()  {
---
+++ 01_introduction/020_factory_method/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"tutorial.masteryourself.pers/tutorial_go/01_introduction/020_factory_method/model"
+)
+
+func main()  {
+ 	var student = model.NewStudent(1, "钢铁侠")
+	fmt.Printf("student = %v \n", student)
+}
