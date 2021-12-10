5eca047d9a9cbbf0f8c047c46968f8a8d5e5c4cc
masteryourself <renzhao.ruan@foxmail.com>
2021-12-10 22:56:25 +0800

    文件copy
    Signed-off-by: = <=>

---
+++ 03_file/04_file_copy/main/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	fmt.Printf("\n==================== 分隔符: copyReadAndWrite ====================\n")
+	copyReadAndWrite()
+	fmt.Printf("\n==================== 分隔符: ioCopy ====================\n")
+	ioCopy()
+}
+
+func copyReadAndWrite() {
+	sourceFilePath := "03_file/04_file_copy/doc/source/hello.txt"
+	destFilePath := "03_file/04_file_copy/doc/dest/hello.txt"
+	data, err := ioutil.ReadFile(sourceFilePath)
+	if err != nil {
+		fmt.Println("文件读取失败", err)
+		return
+	}
+	err = ioutil.WriteFile(destFilePath, data, 0666)
+	if err != nil {
+		fmt.Println("文件写入失败", err)
+	}
+}
+
+func ioCopy() {
+	sourceFile, err := os.Open("03_file/04_file_copy/doc/source/风景.jpeg")
+	if err != nil {
+		fmt.Println("源文件打开失败", err)
+		return
+	}
+	destFile, err := os.OpenFile("03_file/04_file_copy/doc/dest/风景.jpeg", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("目标文件打开失败", err)
+		return
+	}
+	res, err := io.Copy(destFile, sourceFile)
+	if err != nil {
+		fmt.Println("文件 copy 失败")
+	}
+	fmt.Printf("写入字节总数 %v \n", res)
+}
